e94014b640ab90c4e9d8909985fe61121c83a0dc
davidos23 Mac <Mikhail@MacBook-Pro-Mikhail-Sklovskiy.local>
2019-11-21 16:53:33 +0100

    check_parameters, calculate_correlation valtoztatasok

--- Final Code.R
+++ Final Code.R
@@ -7,7 +7,6 @@ return_maker <- function(WTI_fut)
   ## -!-this function is copied from out pprevious work because this code calculated the correlation with prices
   ## in this code i have changed ret_WTI_fut output name to WTI2 because this is the basic parameter in the original code
   #this function imports the dataset as well 
-  #random rtext
   WTI_fut <- readxl::read_excel("WTI2.xlsx")
   n <- nrow(WTI_fut)
   m <- ncol(WTI_fut)
@@ -19,27 +18,32 @@ return_maker <- function(WTI_fut)
       ret_WTI_fut[i,j] <- (WTI_fut[[i,j]] / WTI_fut[[i-1,j]]) - 1
     }
   }
-  
+  # for ciklus 2-tol megy, alapbol a ret matrix a 2 oszlopbol tolti fel. a cbind pedig a détumot berakja a ret elé, de lesz egy ures oszlop
   ret_WTI_fut <- cbind(WTI_fut[,1], ret_WTI_fut)
-  WTI2 <<- ret_WTI_fut[-1,-2]
+  WTI2 <<- data.frame(ret_WTI_fut[-1,-2])
   colnames(WTI2) = colnames(WTI_fut)
+  adat_kezdo<<-WTI2[1,1]
   # View(ret_WTI_fut)
   return(WTI2)
 }
 
+add_parameters <- function(startDate,kesleltet,ablak_meret){
+  kezdo_datum  <<- startDate
+  kesleltet <<- kesleltet
+  ablak_meret <<- ablak_meret
+}
 check_parameters <-
-  function(adat_kezdo,
-           adat_vegso,
-           kezdo_datum,
+  function(kezdo_datum,
            kesleltet,
            ablak_meret) {
-    # Leellenőrizzük, hogy karakter formátumokba kaptuk-e a kezdő dátumot
-    if (typeof(kezdo_datum) != "double") {
-      print("Karakter formátumba adja meg a kezdő és végdátumokat pl: \"2010-01-01\"")
+    
+    # check the type of the start date
+    if (typeof(kezdo_datum) != "character") {
+      print("Character formátumba adja meg a kezdő és végdátumokat pl: \"2010-01-01\"")
       return(FALSE)
       
       # Ha karakterek, akkor megnézzük, hogy a fájlban megadott intervallumba esnek-e
-    } else if (kezdo_datum < adat_kezdo ) {
+    } else if (as.numeric(as.Date(kezdo_datum)) < as.numeric(as.Date(adat_kezdo)) ) {
       print(
         paste(
           "Kérjük olyan dátumot adjon meg, ami az elemzés intervallumába beleesik:"
@@ -55,7 +59,7 @@ check_parameters <-
       
       # check the non-negativity of the the lagg/kesletet
     } else if (kesleltet < 0 ) {
-      print("Kérjük megfelelő intevallumban adja meg a paramétereket (pl. a késleltetés ne legyen negatív).")
+      print("Negatív a késleltetés")
       return(FALSE)
       
     } 
@@ -67,7 +71,8 @@ calculate_correlation <-
            kesleltet = 0,
            the_data = WTI2) {  
     vegso=nrow(WTI2)-1 #-1 as one column kept for the date vector 
-    kezdo_datum_num=as.numeric(adat_kezdo-kezdo_datum)
+    kezdo_datum_num=as.numeric(kezdo_datum-adat_kezdo)
+    #insert adat_kezdo as i forgot it
     m<- vegso - ablak_meret-kezdo_datum_num
     CorMatrixCol=n*(n-1)+2# this is the number of columns that contains correlations +1 as date vector [first one]  
     pairedCorrelation=matrix(nrow=m,ncol=CorMatrixCol)
